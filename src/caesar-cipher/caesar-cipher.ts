51bc85593a24366f2c542cd854512ac13fe547ea
karellehofler <karellehofler@gmail.com>
2022-08-30 20:57:55 -0400

    isMisc function implemented.

--- src/caesar-cipher/caesar-cipher.ts
+++ src/caesar-cipher/caesar-cipher.ts
@@ -11,7 +11,12 @@ function isUpperCase(code : number) {
 }
 
 function isPunctuation(code: number) {
-    if(code > 31 && code < 48) return true;
+    if((code > 31 && code < 48) || (code > 57 && code < 65)) return true;
+    return false;
+}
+
+function isMisc(code: number) {
+    if((code > 90 && code < 97) || (code > 122 && code < 127)) return true;
     return false;
 }
 
@@ -22,11 +27,11 @@ function caesarCipher(value: string, shift: number = 7) {
     let doubleAlphabet = ALPHABET.concat(ALPHABET);
     let res = "";
 
-    for(var i = 0; i < value.length - 1; i++) {
+    for(var i = 0; i <= value.length - 1; i++) {
 
         var code = value.charCodeAt(i);
 
-        if(isNumeric(code) || isPunctuation(code)) {
+        if(isNumeric(code) || isPunctuation(code) || isMisc(code)) {
             res += value[i];
         } else if(isUpperCase(code)) {
             var curr = value[i].toLowerCase();
